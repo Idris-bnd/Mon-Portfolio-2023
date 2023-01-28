224e5704c00ca9e85b4c22e8c06ade7e0fe713c7
idris-bnd <idris.pro83@yahoo.com>
2023-01-28 12:00:53 +0100

    ajout d'assets, creation du premier composant

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+
+function App() {
+
+  return(
+    <section className='App'>
+       Hello mon portfolio
+    </section>
+ )
+}
+export default App;
